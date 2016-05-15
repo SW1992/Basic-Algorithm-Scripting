aaf6d8c127194ae1203261fae32ba83dd68e813f
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-15 11:58:55 +0100

    Add files via upload

---
+++ Check-for-palindromes.js
@@ -0,0 +1,16 @@
+// Check for Palindromes Bonfire Solution
+function palindrome(str) {
+var re = str.replace(/[^a-z0-9]/ig,'');
+var low = re.toLowerCase();
+if (low === low.split("").reverse().join("")) {
+	return true;
+} else {
+	return false; 
+}
+}
+
+
+
+
+
+palindrome("eye");
