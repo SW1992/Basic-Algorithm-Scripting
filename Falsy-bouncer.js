27f4248e09ba08a4da631684ef0ed2de276d2989
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-15 12:46:54 +0100

    Add files via upload

---
+++ Falsy-bouncer.js
@@ -0,0 +1,14 @@
+// Falsy Bouncer Bonfire Solution
+function bouncer(arr) {
+    function isTrue (arr) {
+    return Boolean(arr) === true; 
+  // arr item (converted to boolean) === true
+  }
+  var truthyArray = arr.filter(isTrue);
+  // filter calls callback function isTrue, once for each element of arr
+  /* creates new array of values that returned true & 
+     so passed the test in the callback function */
+  return truthyArray; 
+}
+
+bouncer([7, "ate", "", false, 9]);
