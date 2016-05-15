4ce93af364ad61b3adbfcfc8d89a993310b2704f
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-15 12:52:52 +0100

    Add files via upload

---
+++ Seek-and-destroy.js
@@ -0,0 +1,15 @@
+// Seek and Destroy Bonfire Solution
+function destroyer(arr) { 
+  var arg1 = arguments[1];
+  var arg2 = arguments[2];
+  var arg3 = arguments[3]; 
+  /* You can refer to a function's arguments within the 
+  function, by using the arguments object. */
+  function seek (value) {
+  return value !== arg1 && value !== arg2 && value !== arg3;
+    }
+  var destroy = arr.filter(seek);
+  return destroy;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
