89550ea55872f0cb49fa348e95a171a894e37ff1
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-16 22:31:01 +0100

    Add files via upload

---
+++ Where-do-i-belong.js
@@ -0,0 +1,19 @@
+// Where do i belong Bonfire Solution
+function where(arr, num) {
+  var lowestIndex;
+  function compare(a,b) {
+    return a - b;
+  }
+  arr.sort(compare);
+   for (var i = 0; i < arr.length; i++ ) {
+    if (arr[i] >= num ) {
+     lowestIndex = i;
+     break;
+    }
+    else if (arr[i] < num) {
+      lowestIndex = i + 1;   
+    }
+  }
+  return lowestIndex;
+}
+where([40, 60], 50);
