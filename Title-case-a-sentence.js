a0fcb1ac6b3c9496fdc53fb297d2be67e45b4b58
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-17 10:21:26 +0100

    Add files via upload

---
+++ Title-case-a-sentence.js
@@ -0,0 +1,20 @@
+// Title case a sentence Bonfire Solution
+function titleCase(str) {
+  var arr = str.split(" ");
+  var newArr = [];
+  for (i = 0; i < arr.length;i++) {
+   var firstLetter = arr[i][0];
+   var first = firstLetter.toUpperCase();
+   newArr.push(first);
+   for (x = 1; x < arr[i].length;x++) {
+     var otherLetters = arr[i][x];
+     var other = otherLetters.toLowerCase();
+     newArr.push(other);
+    }
+  }  
+  str = newArr.join(" ");
+  var titleCase = str.replace(/[\s+](?=[a-z'])/g,"");
+  return titleCase;
+} 
+
+titleCase("I'm a little tea pot");
