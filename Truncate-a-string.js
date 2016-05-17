920e74584dba0b70848dd99b84e4ff594aae012a
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-17 10:26:06 +0100

    Update Truncate-a-string.js

--- Truncate-a-string.js
+++ Truncate-a-string.js
@@ -1,16 +1,16 @@
-\\ Truncate a string Bonfire Solution
+// Truncate a string Bonfire Solution
 function truncateString(str, num) {
   if (num <= 3) {
     var truncate = str.slice(0,num);
     return truncate + "...";
     // the length of the three dots is not added to the string length here.
   } else if (str.length > num ) {
-      var truncateStr = str.slice(0,num  - 3); 
-      return truncateStr + "...";
+    var truncateStr = str.slice(0,num  - 3); 
+    return truncateStr + "...";
     // num - 3 accounts for the "..." added onto strings length here
   } else {
     // else (if other two conditions are false) return the str unmodified.
-      return str; 
+    return str; 
   }
 }
 
