f650bd91b56aae025110cff0b1274acca1a1a63b
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-17 10:09:50 +0100

    Add files via upload

---
+++ Repeat-a-string-repeat-a-string.js
@@ -0,0 +1,11 @@
+\\ Repeat a string repeat a string Bonfire Solution
+function repeatStringNumTimes(str, num) {
+  if (num < 0) {
+      return "";
+  } else {
+      var repeats = str.repeat(num);
+      return repeats;
+  }  
+}
+
+repeatStringNumTimes("abc", 3);
