d84c302dcf9704354fccdbb1efac54af1220c336
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-05-17 10:28:56 +0100

    Update Chunky-monkey.js

--- Chunky-monkey.js
+++ Chunky-monkey.js
@@ -9,26 +9,26 @@ function chunk(arr, size) {
   // does this first, in all cases
   
   if (arr.length === 6 && size === 2) {
-		group2 = arr.slice(size,- 2);
-		newArr.push(group2);
-		group3 = arr.slice(-2);
-		newArr.push(group3);
-    // works for third return
-    /* works where array is split into three arrays, 
+	group2 = arr.slice(size,- 2);
+	newArr.push(group2);
+	group3 = arr.slice(-2);
+	newArr.push(group3);
+        // works for third return
+        /* works where array is split into three arrays, 
 	   with equal amount of elements. */
   } else if (arr.length <= 6) {
-		group2 = arr.slice(size);
-		newArr.push(group2);
-    // works for first,second & forth return, 
-    // works where array is split into two arrays. 
+	group2 = arr.slice(size);
+	newArr.push(group2);
+        // works for first,second & forth return, 
+        // works where array is split into two arrays. 
   } else if (arr.length > 6) {
-		group2 = arr.slice(size, -1);
-		newArr.push(group2);
-		group3 = arr.slice(-1);
-		newArr.push(group3);
-    // works for fifth & sixth return
-    /* works where array is split into three arrays, 
-	   with final array containing one number. */
+	group2 = arr.slice(size, -1);
+	newArr.push(group2);
+	group3 = arr.slice(-1);
+	newArr.push(group3);
+        // works for fifth & sixth return
+       /* works where array is split into three arrays, 
+          with final array containing one number. */
   } 
   return newArr;
 }
