aee6320af158e69759a383f2902a704f5aa4e5b1
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-09-01 18:15:07 +0100

    Update Falsy-bouncer.js

--- Falsy-bouncer.js
+++ Falsy-bouncer.js
@@ -1,8 +1,8 @@
 // Falsy Bouncer Bonfire Solution
 function bouncer(arr) {
-    function isTrue (arr) {
+  function isTrue (arr) {
     return Boolean(arr) === true; 
-  // arr item (converted to boolean) === true
+    // arr item (converted to boolean) === true
   }
   var truthyArray = arr.filter(isTrue);
   // filter calls callback function isTrue, once for each element of arr
