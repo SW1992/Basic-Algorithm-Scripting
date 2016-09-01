81673a94c7b3cc0662530270ef33f3adbc319054
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-09-01 18:25:31 +0100

    Update Truncate-a-string.js

--- Truncate-a-string.js
+++ Truncate-a-string.js
@@ -4,11 +4,13 @@ function truncateString(str, num) {
     var truncate = str.slice(0,num);
     return truncate + "...";
     // the length of the three dots is not added to the string length here.
-  } else if (str.length > num ) {
+  } 
+  else if (str.length > num ) {
     var truncateStr = str.slice(0,num  - 3); 
     return truncateStr + "...";
     // num - 3 accounts for the "..." added onto strings length here
-  } else {
+  } 
+  else {
     // else (if other two conditions are false) return the str unmodified.
     return str; 
   }
