f69c1f8c4aaf34f3280390a72d439784f71fc1c5
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-09-01 18:24:14 +0100

    Update Title-case-a-sentence.js

--- Title-case-a-sentence.js
+++ Title-case-a-sentence.js
@@ -3,13 +3,13 @@ function titleCase(str) {
   var arr = str.split(" ");
   var newArr = [];
   for (i = 0; i < arr.length;i++) {
-   var firstLetter = arr[i][0];
-   var first = firstLetter.toUpperCase();
-   newArr.push(first);
-   for (x = 1; x < arr[i].length;x++) {
-     var otherLetters = arr[i][x];
-     var other = otherLetters.toLowerCase();
-     newArr.push(other);
+    var firstLetter = arr[i][0];
+    var first = firstLetter.toUpperCase();
+    newArr.push(first);
+    for (x = 1; x < arr[i].length;x++) {
+      var otherLetters = arr[i][x];
+      var other = otherLetters.toLowerCase();
+      newArr.push(other);
     }
   }  
   str = newArr.join(" ");
