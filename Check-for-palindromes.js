b8d4e419d02104a851a127ab283bc372476abe28
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-09-01 18:13:17 +0100

    Update Check-for-palindromes.js

--- Check-for-palindromes.js
+++ Check-for-palindromes.js
@@ -1,16 +1,13 @@
 // Check for Palindromes Bonfire Solution
 function palindrome(str) {
-var re = str.replace(/[^a-z0-9]/ig,'');
-var low = re.toLowerCase();
-if (low === low.split("").reverse().join("")) {
+  var re = str.replace(/[^a-z0-9]/ig,'');
+  var low = re.toLowerCase();
+  if (low === low.split("").reverse().join("")) {
 	return true;
-} else {
+  } 
+  else {
 	return false; 
+   }
 }
-}
-
-
-
-
 
 palindrome("eye");
